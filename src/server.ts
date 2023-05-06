d74d15fe08128e0758a137b228b6075bf0d0803d
Aron Matheus Garcia Mendes <mmendes@morphus.com.br>
2023-05-06 20:26:20 -0300

    Configurações base

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,12 @@
 // Local
 import { app } from './app'
+import { env } from './env'
 
-app.listen({
-  host: '0.0.0.0',
-  port: 3333
-}).then(() => { console.log('HTTP Server Running 🚀')})
+app
+  .listen({
+    host: '0.0.0.0',
+    port: env.PORT,
+  })
+  .then(() => {
+    console.log('HTTP Server Running 🚀')
+  })
